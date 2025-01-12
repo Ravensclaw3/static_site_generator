5a697243c8b13f7845df5e9f9db6399225684210
Nico van den Berg <ravensclaw3@gmail.com>
2025-01-12 21:38:11 +0200

    chore update unit tests

---
+++ src/test_htmlnode.py
@@ -0,0 +1,19 @@
+import unittest
+
+from htmlnode import HTMLNode
+
+class TestTextNode(unittest.TestCase):
+    def test_to_html(self):
+        self.assertRaises(NotImplementedError, HTMLNode().to_html)
+
+    def props_to_html(self):
+        node = HTMLNode(props={"href": "https://www.google.com", "target": "_blank",})
+        expect = ' href="https://www.google.com" target="_blank"'
+        self.assertEqual(expect, node.props_to_html())
+
+    def test_repr(self):
+        node = HTMLNode("p", "Test HTML node.", None, {"href": "https://www.google.com", "target": "_blank",})
+        self.assertEqual(repr(node), "HTMLNode('p', 'Test HTML node.', 'None', '{'href': 'https://www.google.com', 'target': '_blank'}')")
+
+if __name__ == "__main__":
+    unittest.main()
